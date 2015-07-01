26189ab0aeda1e479a9d29fbb062a50993aa35fd
Leander Jehl <leander.jehl@uis.no>
2015-07-01 12:56:47 +0200

    added a lattice implementation that uses the protobuff messages directly. This safes converting. Not sure though if we should use this one.

---
+++ directCombineLattice/blueprints_slice.go
@@ -0,0 +1,81 @@
+package directCombineLattice
+
+import (
+	pb "github.com/relab/smartMerge/proto"
+)
+
+func sunion(A, B []uint32) (C []uint32) {
+	C = make( []uint32,0, len(A))
+	for _,id := range A {
+		C= append(C, id)
+	}
+	for _,id := range B {
+		copy := true
+		for _, id2 := range A {
+			if id == id2 { 
+				copy = false
+				break
+			}
+		}
+		if copy {
+			C = append (C,id)
+		}
+	}
+	return C
+}
+
+func sdifference(A, B []uint32) (C []uint32) {
+	C = make( []uint32,0,len(A))
+	for _,id := range A {
+		copy := true
+		for _,id2 := range B {
+			if id == id2 {
+				copy = false
+				break
+			}
+		}
+		if copy {
+			C = append (C,id)
+		}
+	}
+	return C
+}
+
+func ssubset(A, B []uint32) bool {
+	for _,id := range A {
+		exists := false
+		for _,id2 := range B {
+			if id == id2 {
+				exists = true
+				break
+			}
+		}
+		if !exists {
+			return false
+		}
+	}
+	return true
+}
+
+func Merge(bp, blpr pb.Blueprint) (mbp pb.Blueprint) {
+	mbp.Rem = sunion(bp.Rem,blpr.Rem)
+	mbp.Add = sdifference(sunion(bp.Add,blpr.Add),mbp.Rem)
+	return mbp
+}
+
+// a.Compare b = 1 <=> a <= b
+// a.Compare b = -1 <=> b < a
+// a.Compare b = 0 <=> !(b <= a) && !(a <= b)
+func Compare(bp, blpr pb.Blueprint) int {
+	if ssubset( bp.Add, sunion( blpr.Add, blpr.Rem)) && ssubset( bp.Rem, blpr.Rem) {
+		return 1
+	}
+	if ssubset( blpr.Add, sunion( bp.Rem, bp.Add)) && ssubset( blpr.Rem, bp.Rem) {
+		return -1
+	}
+	return 0
+}
+
+func Equals(bp, blpr pb.Blueprint) bool {
+	return Compare(bp,blpr) == 1 && Compare( blpr, bp) == 1
+}
---
+++ directCombineLattice/blueprints_slice_test.go
@@ -0,0 +1,112 @@
+package directCombineLattice
+
+import (
+	"testing"
+	pb "github.com/relab/smartMerge/proto"
+	//"fmt"
+)
+
+var onei = uint32(1)
+var twoi = uint32(2)
+var trei = uint32(3)
+
+var a0i = []uint32{}
+var a1i = []uint32{onei, twoi}
+var a2i = []uint32{onei, trei}
+var r1i = []uint32{onei}
+var r2i = []uint32{trei}
+var a3i = []uint32{twoi,trei}
+
+func TestSUnion(t *testing.T) {
+	u1 := sunion(r1i,r2i)
+	for _,id := range u1 {
+		if (id != onei) && id != trei {
+			t.Error("Union did introduce new element.")
+		}
+	}
+	if len(u1) != 2 {
+		t.Error("Union does not have the right number of elements.")
+	}
+
+	u1 = sunion(a1i,a2i)
+
+	for _,id := range u1 {
+		if id != onei && id != trei && id != twoi {
+			t.Error("Union did introduce new element.")
+		}
+	}
+	if len(u1) != 3 {
+		t.Error("Union does not have the right number of elements.")
+	}
+}
+
+func TestSDifference(t *testing.T) {
+	df := sdifference(a2i,r2i)
+	for _,id := range df {
+		if id != onei {
+			t.Error("Unwanted element in sdifference.")
+		}
+	}
+	if len(df) != 1 {
+		t.Error("Difference does not have the right number of elements.")
+	}
+
+	df = sdifference(a1i,a2i)
+
+	if len(df) != 1 {
+		t.Error("Difference does not have the right number of elements.")
+	}
+	for _,id := range df {
+		if id != twoi {
+			t.Errorf("Difference(%v,%v) was %v, not ID(2).",a1i, a2i, df)
+		}
+	}
+}
+
+func TestSSubset(t *testing.T) {
+	if !ssubset(r1i, a1i) {
+		t.Errorf("%v was no ssubset of %v.", r1i, a1i)
+	}
+	if ssubset(a2i,a1i) {
+		t.Errorf("%v was ssubset of %v.", a2i, a1i)
+	}
+}
+
+var bpi1 = pb.Blueprint{r1i, r2i}
+var bpi2 = pb.Blueprint{a1i, r2i}
+var bpi3 = pb.Blueprint{a2i, a0i}
+var bpix = pb.Blueprint{r2i, r1i}
+var bpiy = pb.Blueprint{a1i,a0i}
+var bpiz = pb.Blueprint{a3i,r1i}
+
+func TestSCompar(t *testing.T) {
+	if Compare(bpi1,bpi2) != 1 {
+		t.Errorf("%v not smaller %v.", bpi1, bpi2)
+	}
+	if Compare(bpi2,bpi1) != -1 {
+		t.Errorf("%v not larger %v.", bpi2, bpi1)
+	}
+	if Compare(bpix,bpiy) != 0 {
+		t.Errorf("%v comparable to %v.", bpix, bpiy)
+	}
+	if Compare(bpi3,bpi1) != 1 {
+		t.Errorf("%v not larger %v.", bpi3, bpi1)
+	}
+
+}
+
+
+func TestSMerge(t *testing.T) {
+	m := Merge(bpi1,bpi2)
+	if !Equals(m,bpi2) {
+		t.Errorf("merge(%v, %v) was ¤v.", bpi1, bpi2, m)
+	}
+	m = Merge(bpi1,bpi3)
+	if !Equals(m,bpi1) {
+		t.Errorf("merge(%v, %v) was ¤v.", bpi1, bpi3, m)
+	}
+	m = Merge(bpix,bpiy)
+	if !Equals(m,bpiz) {
+		t.Errorf("merge(%v, %v) was ¤v.", bpi1, bpi2, m)
+	}
+}

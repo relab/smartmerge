1c91718e1a2fba29fe03e9e238c9159a895ca32e
Leander Jehl <leander.jehl@uis.no>
2015-07-24 10:40:38 +0200

    efmt

--- elog/util/efmt/efmt.go
+++ elog/util/efmt/efmt.go
@@ -3,14 +3,17 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
 	"os"
+	"time"
 
-	"github.com/relab/goxos/elog/event"
+	e "github.com/relab/smartMerge/elog/event"
 )
 
 func main() {
 	var file = flag.String("file", "", "elog file to parse")
-	var filter = flag.Bool("filter", true, "filter out throughput samples")
+	//var filter = flag.Bool("filter", true, "filter out throughput samples")
+	var outfile = flag.String("outfile", "", "write results to file")
 
 	flag.Usage = func() {
 		fmt.Fprintf(os.Stderr, "Usage: %s [OPTIONS]\n", os.Args[0])
@@ -25,17 +28,97 @@ func main() {
 		os.Exit(1)
 	}
 
-	events, err := event.Parse(*file)
+	var of io.Writer
+	if *outfile == "" {
+		of = os.Stderr
+	} else {
+		of, err := os.Create(*outfile)
+		if err != nil {
+			fmt.Println("Could not create file: ", *outfile)
+			return
+		}
+		defer of.Close()
+
+	}
+
+	events, err := e.Parse(*file)
 	if err != nil {
 		fmt.Println("Error parsing events:", err)
 		return
 	}
 
-	if *filter {
-		events, _ = event.ExtractThroughput(events)
+	readl, writel, reconfl := sortLatencies(events)
+	if len(readl) > 0 {
+		fmt.Fprintln(of, "Read Latencies:")
+		for k, durs := range readl {
+			avg := MeanDuration(durs...)
+			fmt.Fprintf(of, "Accesses %2d, %5d times, AvgLatency: %v\n", k, len(durs), avg)
+		}
 	}
+	if len(writel) > 0 {
+		fmt.Fprintln(of, "Write Latencies:")
+		for k, durs := range writel {
+			avg := MeanDuration(durs...)
+			fmt.Fprintf(of, "Accesses %2d, %5d times, AvgLatency: %v\n", k, len(durs), avg)
+		}
+	}
+	if len(reconfl) > 0 {
+		fmt.Fprintln(of, "Reconf Latencies:")
+		for k, durs := range reconfl {
+			avg := MeanDuration(durs...)
+			fmt.Fprintf(of, "Accesses %2d, %5d times, AvgLatency: %v\n", k, len(durs), avg)
+		}
+	}
+}
 
-	for i, event := range events {
-		fmt.Printf("%2d: %v\n", i, event)
+func sortLatencies(events []e.Event) (readl map[uint64][]time.Duration, writel map[uint64][]time.Duration, reconfl map[uint64][]time.Duration) {
+	readl = make(map[uint64][]time.Duration, 0)
+	writel = make(map[uint64][]time.Duration, 0)
+	reconfl = make(map[uint64][]time.Duration, 0)
+	for _, evt := range events {
+		switch evt.Type {
+		case e.ClientReadLatency:
+			if readl[evt.Value] == nil {
+				readl[evt.Value] = []time.Duration{evt.EndTime.Sub(evt.Time)}
+			} else {
+				readl[evt.Value] = append(readl[evt.Value], evt.EndTime.Sub(evt.Time))
+			}
+		case e.ClientWriteLatency:
+			if writel[evt.Value] == nil {
+				writel[evt.Value] = []time.Duration{evt.EndTime.Sub(evt.Time)}
+			} else {
+				writel[evt.Value] = append(writel[evt.Value], evt.EndTime.Sub(evt.Time))
+			}
+		case e.ClientReconfLatency:
+			if reconfl[evt.Value] == nil {
+				reconfl[evt.Value] = []time.Duration{evt.EndTime.Sub(evt.Time)}
+			} else {
+				reconfl[evt.Value] = append(reconfl[evt.Value], evt.EndTime.Sub(evt.Time))
+			}
+		}
+
 	}
+	return
+}
+
+func computeAverageDuration(durs map[uint64][]time.Duration) map[uint64]time.Duration {
+	if durs == nil {
+		return nil
+	}
+	avgs := make(map[uint64]time.Duration, len(durs))
+	for k, ds := range durs {
+		avgs[k] = MeanDuration(ds...)
+	}
+	return avgs
+}
+
+func MeanDuration(v ...time.Duration) time.Duration {
+	if len(v) == 0 {
+		return 0
+	}
+	var sum time.Duration
+	for _, dur := range v {
+		sum += dur
+	}
+	return sum / time.Duration((len(v)))
 }

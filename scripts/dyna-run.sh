8277c5135b9772e25852583b3732f6d04e7c1b9c
Leander Jehl <leander.jehl@uis.no>
2015-07-29 09:14:37 +0200

    stuff

---
+++ scripts/dyna-run.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+echo starting servers.
+ssh pitter24 "$HOME/mygo/src/github.com/relab/smartMerge/server/dynaservers.sh" &
+ssh pitter25 "$HOME/mygo/src/github.com/relab/smartMerge/server/dynaservers.sh" &
+ssh pitter26 "$HOME/mygo/src/github.com/relab/smartMerge/server/dynaservers.sh" &
+
+export SM=$HOME/mygo/src/github.com/relab/smartMerge
+
+sleep 1
+
+cd $SM
+
+echo starting Writers
+(client/client -conf client/addrList -alg=dyna -mode=bench -contW -size=4000 -nclients=5 -id=5 -initsize=12 -gc-off > logfile &)
+
+echo starting Reconfigurers
+client/client -conf client/addrList -alg=dyna -mode=exp -rm -nclients=2 -initsize=12 -gc-off -elog
+
+sleep 1
+echo stopping Writers
+killall client/client 
+ 
+ssh pitter24 "pkill -u ljehl"
+ssh pitter25 "pkill -u ljehl"
+ssh pitter26 "pkill -u ljehl"
+

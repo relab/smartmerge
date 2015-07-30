f3fed0f65592cf40350820e5773c507dc1a9353f
Leander Jehl <leander.jehl@uis.no>
2015-07-30 15:51:25 +0200

    exp

--- scripts/expertiment.sh
+++ scripts/expertiment.sh
@@ -10,10 +10,10 @@ do
 	./sm-run.sh 2
 done
 cd $SM
-mkdir sm_rm2
-mv *.elog sm_rm2/ 
-cd sm_rm2
-$SM/scripts/analyzeall sm_remove2
+mkdir "sm_rm2$*"
+mv *.elog "sm_rm2$*/" 
+cd "sm_rm2$*"
+$SM/scripts/analyzeall "sm_remove2$*"
 
 cd ../scripts
 for i in {1..20} 
@@ -21,11 +21,11 @@ do
 	./dyna-run.sh 2
 done
 cd $SM
-mkdir dyna_rm2
-mv *.elog dyna_rm2/ 
-cd dyna_rm2
+mkdir "dyna_rm2$*"
+mv *.elog "dyna_rm2$*/" 
+cd "dyna_rm2$*"
 echo Analyzing
-$SM/scripts/analyzeall dyna_remove2
+$SM/scripts/analyzeall "dyna_remove2$*"
 
 cd $SM/scripts
 for i in {1..20} 
@@ -34,10 +34,10 @@ do
 	./sm-run.sh 5
 done
 cd $SM
-mkdir sm_rm5
-mv *.elog sm_rm5/ 
-cd sm_rm5
-$SM/scripts/analyzeall sm_remove5
+mkdir "sm_rm5$*"
+mv *.elog "sm_rm5$*/" 
+cd "sm_rm5$*"
+$SM/scripts/analyzeall "sm_remove5$*"
 
 cd ../scripts
 for i in {1..20} 
@@ -45,10 +45,10 @@ do
 	./dyna-run.sh 5
 done
 cd $SM
-mkdir dyna_rm5
-mv *.elog dyna_rm5/ 
-cd dyna_rm5
+mkdir "dyna_rm5$*"
+mv *.elog "dyna_rm5$*/" 
+cd "dyna_rm5$*"
 echo Analyzing
-$SM/scripts/analyzeall dyna_remove5
+$SM/scripts/analyzeall "dyna_remove5$*"
 
 

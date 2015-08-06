df01d62ce3e538a14e09fcd75510cb45a8e1da63
Leander Jehl <leander.jehl@uis.no>
2015-08-06 09:18:15 +0200

    include back original traverse

--- dynaclient/traverse.go
+++ dynaclient/traverse.go
@@ -11,22 +11,26 @@ import (
 
 func (dc *DynaClient) Traverse(prop *lat.Blueprint, val []byte) ([]byte, int, error) {
 	cnt := 0
+	cur := 0
 	rst := new(pb.State)
 	for i := 0; i < len(dc.Confs); i++ {
+		if i < cur {
+			continue
+		}
+		var curprop *lat.Blueprint
 		if prop != nil && !prop.Equals(dc.Blueps[i]) {
 			//Update Snapshot
-/*			if dc.Blueps[i].Compare(prop) != 1 {
-				fmt.Println("target blueprint is not greater then current blueprint")
-				return nil, cnt, errors.New("target not comparable to current")
-			}
-*/			
+			/*			if dc.Blueps[i].Compare(prop) != 1 {
+							fmt.Println("target blueprint is not greater then current blueprint")
+							return nil, cnt, errors.New("target not comparable to current")
+						}
+			*/
 			next, newCur, err := dc.Confs[i].GetOneN(dc.Blueps[i], prop)
 			//fmt.Println("invoke getone")
 			cnt++
-			restart := dc.handleNewCur(newCur)
-			if restart {
-				prop = prop.Merge(newCur)
-				i = -1
+			cur = dc.handleNewCur(cur, i, newCur)
+			prop = prop.Merge(newCur)
+			if i < cur {
 				continue
 			}
 
@@ -34,14 +38,16 @@ func (dc *DynaClient) Traverse(prop *lat.Blueprint, val []byte) ([]byte, int, er
 				fmt.Println("Error from GetOneN")
 				return nil, 0, err
 			}
+			
+			curprop = next
 
-			newCur, err = dc.Confs[i].DWriteNSet([]*lat.Blueprint{next}, dc.Blueps[i])
+			//A possible optimization would combine this WriteN with the ReadS below
+/*			newCur, err = dc.Confs[i].DWriteNSet([]*lat.Blueprint{next}, dc.Blueps[i])
 			//fmt.Println("invoke writeN")
 			cnt++
-			restart = dc.handleNewCur(newCur)
-			if restart {
-				prop = prop.Merge(newCur)
-				i = -1
+			cur = dc.handleNewCur(cur, i, newCur)
+			prop = prop.Merge(newCur)
+			if i < cur {
 				continue
 			}
 
@@ -49,16 +55,19 @@ func (dc *DynaClient) Traverse(prop *lat.Blueprint, val []byte) ([]byte, int, er
 				fmt.Println("Error from DWriteNSet")
 				return nil, 0, err
 			}
+*/
 		}
 
 		//ReadInView:
-		st, _, newCur, err := dc.Confs[i].DReadS(dc.Blueps[i], nil)
+		//This already is an optimization. Could be split in reads and readN
+		st, next, newCur, err := dc.Confs[i].DReadS(dc.Blueps[i], curprop)
 		//fmt.Println("invoke readS")
 		cnt++
-		restart := dc.handleNewCur(newCur)
-		if restart {
+		cur = dc.handleNewCur(cur, i, newCur)
+		if prop != nil || len(next) > 0 {
 			prop = prop.Merge(newCur)
-			i = -1
+		}
+		if i < cur {
 			continue
 		}
 		if err != nil {
@@ -66,21 +75,13 @@ func (dc *DynaClient) Traverse(prop *lat.Blueprint, val []byte) ([]byte, int, er
 			return nil, 0, err
 		}
 
-		//Using DWriteS here is a shortcut, but I think it works just fine.
-		next, newCur, err := dc.Confs[i].DWriteS(nil, dc.Blueps[i])
-		//fmt.Println("invoke readN")
-		cnt++
-		restart = dc.handleNewCur(newCur)
-		if restart {
-			prop = prop.Merge(newCur)
-			i = -1
-			continue
-		}
-		if err != nil {
-			fmt.Println("Error from DReadN")
-			return nil, 0, err
-		}
-
+		/*		for _,nxt := range next {
+					if dc.Blueps[i].Compare(nxt) != 1{
+						fmt.Println("Returned next, that is not greater than this")
+						panic("Next not comparable.")
+					}
+				}
+		*/
 		prop = dc.handleNext(i, next, prop)
 		if rst.Compare(st) == 1 {
 			rst = st
@@ -105,29 +106,14 @@ func (dc *DynaClient) Traverse(prop *lat.Blueprint, val []byte) ([]byte, int, er
 		if len(next) == 0 {
 			//WriteInView
 			wst := dc.WriteValue(val, rst)
-			_, newCur, err = dc.Confs[i].DWriteS(wst, dc.Blueps[i])
+			next, newCur, err = dc.Confs[i].DWriteS(wst, dc.Blueps[i])
 			//fmt.Println("invoke writeS")
 			cnt++
-			restart = dc.handleNewCur(newCur)
-			if restart {
+			cur = dc.handleNewCur(cur, i, newCur)
+			if prop != nil || len(next) > 0 {
 				prop = prop.Merge(newCur)
-				i = -1
-				continue
 			}
-
-			if err != nil {
-				fmt.Println("Error from DWriteS")
-				return nil, 0, err
-			}
-
-			//Using DWriteS here is a shortcut, but I think it works just fine.
-			next, newCur, err := dc.Confs[i].DWriteS(nil, dc.Blueps[i])
-			//fmt.Println("invoke readN")
-			cnt++
-			restart = dc.handleNewCur(newCur)
-			if restart {
-				prop = prop.Merge(newCur)
-				i = -1
+			if i < cur {
 				continue
 			}
 
@@ -137,20 +123,16 @@ func (dc *DynaClient) Traverse(prop *lat.Blueprint, val []byte) ([]byte, int, er
 			}
 
 			prop = dc.handleNext(i, next, prop)
-
 		}
 
 		if len(next) > 0 {
 			newCur, err = dc.Confs[i].DWriteNSet(next, dc.Blueps[i])
 			//fmt.Println("invoke writeN")
 			cnt++
-			restart = dc.handleNewCur(newCur)
-			if restart {
+			cur = dc.handleNewCur(cur, i, newCur)
+			if prop != nil {
 				prop = prop.Merge(newCur)
-				i = -1
-				continue
 			}
-
 			if err != nil {
 				fmt.Println("Error from DWriteNSet")
 				return nil, 0, err
@@ -159,15 +141,15 @@ func (dc *DynaClient) Traverse(prop *lat.Blueprint, val []byte) ([]byte, int, er
 		}
 	}
 
-	i := len(dc.Confs) -1
-	if i > 0  {
+	if i := len(dc.Confs) - 1; i > cur {
 		dc.Confs[i].DSetCur(dc.Blueps[i])
 		//fmt.Println("setcur")
 		cnt++
+		cur = i
 	}
 
-	dc.Blueps = dc.Blueps[i:]
-	dc.Confs = dc.Confs[i:]
+	dc.Blueps = dc.Blueps[cur:]
+	dc.Confs = dc.Confs[cur:]
 
 	if val == nil {
 		return rst.Value, cnt, nil
@@ -175,33 +157,26 @@ func (dc *DynaClient) Traverse(prop *lat.Blueprint, val []byte) ([]byte, int, er
 	return nil, cnt, nil
 }
 
-func (dc *DynaClient) handleNewCur(newCur *lat.Blueprint) bool {
-	if newCur.Compare(dc.Blueps[0]) == 1 {
-		return false
+func (dc *DynaClient) handleNewCur(cur int, i int, newCur *lat.Blueprint) int {
+	if newCur == nil {
+		return cur
 	}
-	found := false
-	cur := 1
-	for ; cur < len(dc.Blueps); cur++ {
-		if newCur.Compare(dc.Blueps[cur]) == 1 {
-			if dc.Blueps[cur].Compare(newCur) == 1 {
-				found = true
+	if newCur.Compare(dc.Blueps[i]) == 1 {
+		return cur
+	}
+	cur, remove := dc.findorinsert(i, newCur)
+	if remove {
+		for ; cur < len(dc.Blueps)-1; cur++ {
+			if dc.Blueps[cur+1].Compare(dc.Blueps[cur]) == 0 {
+				dc.Blueps[cur+1] = dc.Blueps[cur]
+				dc.Confs[cur+1] = dc.Confs[cur]
+			} else {
+				break
 			}
-			break
 		}
 	}
+	return cur
 
-	if found {
-		dc.Blueps = dc.Blueps[cur:cur+1]
-		dc.Confs = dc.Confs[cur:cur+1]
-	} else {
-		cnf, err := dc.mgr.NewConfiguration(newCur.Ids(), majQuorum(newCur))
-		if err != nil {
-			panic("could not get new config")
-		}
-		dc.Blueps = []*lat.Blueprint{newCur}
-		dc.Confs = []*rpc.Configuration{cnf}
-	}
-	return true
 }
 
 func (dc *DynaClient) handleNext(i int, next []*lat.Blueprint, prop *lat.Blueprint) *lat.Blueprint {

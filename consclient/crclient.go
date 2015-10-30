d2049b6dbbe73538735fd0e440f4373e623019e9
leanderjehl <leander.jehl@uis.no>
2015-10-30 21:05:35 +0100

    consensus-r-client

---
+++ consclient/crclient.go
@@ -0,0 +1,69 @@
+package consclient
+
+import (
+	"github.com/golang/glog"
+	pb "github.com/relab/smartMerge/proto"
+)
+
+type CRClient struct {
+	*CClient
+}
+
+func NewCR(initBlp *pb.Blueprint, mgr *pb.Manager, id uint32) (*CRClient, error) {
+	cc, err := New(initBlp, mgr, id)
+	return &CRClient{cc}, err
+}
+
+//Atomic read
+func (cr *CRClient) Read() (val []byte, cnt int) {
+	if glog.V(5) {
+		glog.Infoln("starting Read")
+	}
+	st, cnt, err := cr.reconf(nil, false, nil)
+	if err != nil {
+		glog.Errorln("Error during Read")
+		return nil, 0
+	}
+
+	if glog.V(3) {
+		if cnt > 2 {
+			glog.Infof("Read used %d accesses\n", cnt)
+		}
+	}
+	return st.Value, cnt
+}
+
+//Regular read
+func (cr *CRClient) RRead() (val []byte, cnt int) {
+	if glog.V(5) {
+		glog.Infoln("starting regular Read")
+	}
+	st, cnt, err := cr.reconf(nil, true, nil)
+	if err != nil {
+		glog.Errorln("Error during RRead")
+		return nil, 0
+	}
+	if glog.V(3) {
+		if cnt > 1 {
+			glog.Infof("RRead used %d accesses\n", cnt)
+		}
+	}
+	return st.Value, cnt
+}
+
+func (cr *CRClient) Write(val []byte) int {
+	if glog.V(5) {
+		glog.Infoln("starting Write")
+	}
+	_, cnt, err := cr.reconf(nil, false, val)
+	if err != nil {
+		glog.Errorln("Error during Write")
+		return 0
+	}
+	if glog.V(3) {
+		if cnt > 2 {
+			glog.Infof("Write used %d accesses\n", cnt)
+		}
+	}
+	return cnt
+}
--- consclient/reconf.go
+++ consclient/reconf.go
@@ -9,11 +9,11 @@ import (
 )
 
 func (cc *CClient) Reconf(prop *pb.Blueprint) (cnt int, err error) {
-	_, cnt, err = cc.reconf(prop, false, nil)
+	_, cnt, err = cc.reconf(prop, true, nil)
 	return
 }
 
-func (cc *CClient) reconf(prop *pb.Blueprint, regular bool, inst *pb.State) (rst *pb.State, cnt int, err error) {
+func (cc *CClient) reconf(prop *pb.Blueprint, regular bool, val []byte) (rst *pb.State, cnt int, err error) {
 	if glog.V(2) {
 		glog.Infof("C%d: Starting reconfiguration\n", cc.ID)
 	}
@@ -25,43 +25,43 @@ func (cc *CClient) reconf(prop *pb.Blueprint, regular bool, inst *pb.State) (rst
 
 	if len(prop.Add) == 0 {
 		glog.Errorf("Aborting Reconfiguration to avoid unacceptable configuration.")
-		return cnt, errors.New("Abort before proposing unacceptable configuration.")
+		return nil, cnt, errors.New("Abort before proposing unacceptable configuration.")
 	}
 
 	cur := 0
-	forconfiguration:
+forconfiguration:
 	for i := 0; i < len(cc.Confs); i++ {
 		if i < cur {
 			continue
 		}
 
 		var next *pb.Blueprint
-		
+
 		switch prop.Compare(cc.Blueps[i]) {
 		case 0, -1:
 			//Need to agree on new proposal
 			var cs int
 			next, cs, cur, err = cc.getconsensus(i, prop)
 			if err != nil {
-				return 0, err
+				return nil, 0, err
 			}
 			cnt += cs
 		case 1:
 			// No proposal
 			var st *pb.State
-			st, next, cur, err = cc.doread(cur,i)
+			st, next, cur, err = cc.doread(cur, i)
 			if err != nil {
-				return 0, err
+				return nil, 0, err
 			}
 			cnt++
 			if rst.Compare(st) == 1 {
 				rst = st
-			}		 
+			}
 		}
 		if i < cur {
 			continue forconfiguration
 		}
-	
+
 		if cc.Blueps[i].LearnedCompare(next) == 1 {
 			readS, err := cc.Confs[i].CWriteN(&pb.DRead{CurC: uint32(cc.Blueps[i].Len()), Prop: next})
 			if glog.V(3) {
@@ -72,7 +72,7 @@ func (cc *CClient) reconf(prop *pb.Blueprint, regular bool, inst *pb.State) (rst
 			if err != nil && cur <= i {
 				glog.Errorf("C%d: error from CReadS: %v\n", cc.ID, err)
 				//No Quorum Available. Retry
-				return 0, err
+				return nil, 0, err
 			}
 
 			for _, next = range readS.Reply.GetNext() {
@@ -81,15 +81,18 @@ func (cc *CClient) reconf(prop *pb.Blueprint, regular bool, inst *pb.State) (rst
 
 			if rst.Compare(readS.Reply.GetState()) == 1 {
 				rst = readS.Reply.GetState()
-			}			
-			
+			}
+
 		} else if next != nil {
 			glog.Errorln("This case should never happen. There might be a bug in the code.")
 		}
-		
+
 	}
 
-	if i := len(cc.Confs) - 1; i > cur {
+	if i := len(cc.Confs) - 1; i > cur || !regular {
+
+		rst = cc.WriteValue(val, rst)
+
 		_, err := cc.Confs[i].CSetState(&pb.CNewCur{Cur: cc.Blueps[i], CurC: uint32(cc.Blueps[i].Len()), State: rst})
 		if glog.V(3) {
 			glog.Infof("C%d: Set state in configuration of size %d.\n", cc.ID, cc.Blueps[i].Len())
@@ -98,7 +101,7 @@ func (cc *CClient) reconf(prop *pb.Blueprint, regular bool, inst *pb.State) (rst
 		if err != nil {
 			//Not sure what to do:
 			glog.Errorf("C%d: SetState returned error, not sure what to do\n", cc.ID)
-			return 0, err
+			return nil, 0, err
 		}
 		cur = i
 	}
@@ -106,38 +109,32 @@ func (cc *CClient) reconf(prop *pb.Blueprint, regular bool, inst *pb.State) (rst
 	cc.Blueps = cc.Blueps[cur:]
 	cc.Confs = cc.Confs[cur:]
 
-	return cnt, nil
+	return rst, cnt, nil
 }
-
-
-
 
 func (cc *CClient) handleOneCur(cur int, newCur *pb.Blueprint) int {
 	if newCur == nil {
 		return cur
 	}
-	
+
 	if glog.V(3) {
 		glog.Infof("C%d: Found new Cur with length %d, current has length %d\n", cc.ID, newCur.Len(), cc.Blueps[cur].Len())
 	}
 	return cc.findorinsert(cur, newCur)
-	
+
 }
 
-
-
-
 func (cc *CClient) getconsensus(i int, prop *pb.Blueprint) (next *pb.Blueprint, cnt, cur int, err error) {
-	ms := 1 * time.Millisecond	
+	ms := 1 * time.Millisecond
 	rnd := cc.ID
-	prepare:	
+prepare:
 	for {
 		//Send Prepare:
 		promise, errx := cc.Confs[i].CPrepare(&pb.Prepare{CurC: uint32(cc.Blueps[i].Len()), Rnd: rnd})
 		if errx != nil {
 			//Should log this for debugging
 			glog.Errorf("C%d: Prepare returned error: %v\n", cc.ID, errx)
-			return nil,0, i, errx
+			return nil, 0, i, errx
 		}
 		cnt++
 		cur = cc.handleOneCur(i, promise.Reply.GetCur())
@@ -163,7 +160,7 @@ func (cc *CClient) getconsensus(i int, prop *pb.Blueprint) (next *pb.Blueprint, 
 			} else {
 				next = prop.Merge(cc.Blueps[i])
 				if glog.V(3) {
-					glog.Infof("C%d: Proposing my value.\n", cc.ID )
+					glog.Infof("C%d: Proposing my value.\n", cc.ID)
 				}
 			}
 		case rrnd > rnd:
@@ -179,15 +176,15 @@ func (cc *CClient) getconsensus(i int, prop *pb.Blueprint) (next *pb.Blueprint, 
 			time.Sleep(ms)
 			ms = 2 * ms
 			continue prepare
-			
+
 		}
 
 		learn, errx := cc.Confs[i].CAccept(&pb.Propose{CurC: uint32(cc.Blueps[i].Len()), Val: &pb.CV{rnd, next}})
 		if err != nil {
-			glog.Errorf("C%d: Accept returned error: %v\n", cc.ID , errx)
-			return nil,0, cur, errx
+			glog.Errorf("C%d: Accept returned error: %v\n", cc.ID, errx)
+			return nil, 0, cur, errx
 		}
-	
+
 		cnt++
 		cur = cc.handleOneCur(cur, learn.Reply.GetCur())
 		if i < cur {
@@ -206,7 +203,7 @@ func (cc *CClient) getconsensus(i int, prop *pb.Blueprint) (next *pb.Blueprint, 
 		if learn.Reply.GetDec() != nil {
 			next = learn.Reply.GetDec()
 		}
-		
+
 		return
 	}
 }
@@ -219,14 +216,20 @@ func (cc *CClient) doread(curin, i int) (st *pb.State, next *pb.Blueprint, cur i
 		//return
 	}
 	if glog.V(4) {
-		glog.Infof("C%d: CReadS returned with replies from \n", cc.ID , read.MachineIDs)
+		glog.Infof("C%d: CReadS returned with replies from \n", cc.ID, read.MachineIDs)
 	}
 	cur = cc.handleNewCur(curin, read.Reply.GetCur())
-	
+
 	for _, next = range read.Reply.GetNext() {
 		cc.handleNext(i, next)
 	}
-	
-	return	
+
+	return
 }
 
+func (cc *CClient) WriteValue(val []byte, st *pb.State) *pb.State {
+	if val == nil {
+		return st
+	}
+	return &pb.State{Value: val, Timestamp: st.Timestamp + 1, Writer: cc.ID}
+}

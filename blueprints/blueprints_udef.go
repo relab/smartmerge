1b1146a0b98579a73e5a81d96790bea12adc56f6
Leander Jehl <leander.jehl@uis.no>
2017-01-27 11:06:09 +0100

    go generate

--- blueprints/blueprints_udef.go
+++ blueprints/blueprints_udef.go
@@ -3,6 +3,9 @@ package blueprints
 // This file contains manually coded methods on the blueprint structs created
 // from protobuf.
 
+// To gernerate the code from protobuf run go generate in this folder
+//go:generate protoc --gofast_out=. blueprints.proto
+
 // Difference computes the difference between two arrays,
 // i.e. all elements from A, that are not part of B.
 // It is used in the norecontact-configuration provider.
